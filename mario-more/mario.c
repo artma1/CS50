6404e5207f955d0fd357880b4dd043f6c7533b69
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-14 18:15:37 +0000

    Thu, Mar 14, 2024, 6:15 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,8 +4,9 @@
 int main (void)
 {
     int n = get_int("Chose the size of the pyramid" );
-    if(n<1 || n>8)
+   do
     {
         printf("The size must be between 1 and 8\n");
     }
+    while(n<1 || n>8)
 }
