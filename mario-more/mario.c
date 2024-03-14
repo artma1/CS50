59b39e4bf8e8cee2792cf8bf301307f17cc1f8cc
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-14 18:49:48 +0000

    Thu, Mar 14, 2024, 6:49 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,12 +3,14 @@
 
 int main (void)
 {
+    //diz que n é um integral, recebe n do usuário
     int n;
-
     do
     {
     n = get_int("chose the size of the pyramid ");
     printf("The size must be between 1 and 8\n");
     }
     while (n<1||n>8);
+    for (int i=0, i<n, i++)
+    
 }
