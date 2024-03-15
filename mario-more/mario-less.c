5e2d2f8fa67881e052d928cbf56da021c6e4d09f
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-15 01:15:30 +0000

    Thu, Mar 14, 2024, 10:15 PM -03:00

--- mario-more/mario-less.c
+++ mario-more/mario-less.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
-int n
-do
-int n = get_int("Chose the pyramid size" )
-if
-while(n>0)
+int main(void);
+{
+int n;
+    do
+        {
+        int n = get_int("Chose the pyramid size" );
+        if (n<0)
+        printf("Size must be bigger than 0");
+        }
+    while(n<0);
+}
 
