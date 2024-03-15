a640a21f7c55deb511e5b8a7ecbb6fb77b627191
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-15 16:23:32 +0000

    Fri, Mar 15, 2024, 1:23 PM -03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,41 +1,41 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main (void)
+int main(void)
 {
-    //ask user for size, limit between 1 and 8
+    // ask user for size, limit between 1 and 8
     int n;
     do
     {
-    n = get_int("chose the size of the pyramid ");
-    if (n<1||n>8)
+        n = get_int("chose the size of the pyramid ");
+        if (n < 1 || n > 8)
         {
-        printf("The size must be between 1 and 8\n");
+            printf("The size must be between 1 and 8\n");
         }
     }
-    while (n<1||n>8);
-    for (int i=1; i<=n; i++)
+    while (n < 1 || n > 8);
+    for (int i = 1; i <= n; i++)
     {
-        //first half of pyramid
-        for (int j=i; j<n; j++)
+        // first half of pyramid
+        for (int j = i; j < n; j++)
         {
             printf(" ");
         }
-        for (int k=1; k<=i; k++)
+        for (int k = 1; k <= i; k++)
         {
             printf("#");
         }
-        //space between the two parts
-        for (int l=1; l<=2; l++)
+        // space between the two parts
+        for (int l = 1; l <= 2; l++)
         {
             printf(" ");
         }
-        //second half of pyramid
-        for (int o=1; o<=i; o++)
+        // second half of pyramid
+        for (int o = 1; o <= i; o++)
         {
             printf("#");
         }
-        for (int m=i; m<n; m++)
+        for (int m = i; m < n; m++)
         {
             printf(" ");
         }
