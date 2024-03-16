26ec22b27f142ba81d449257d37442997cead9d6
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-16 23:22:26 +0000

    Sat, Mar 16, 2024, 8:22 PM -03:00

--- cash/cash2.c
+++ cash/cash2.c
@@ -5,8 +5,9 @@ int main(void)
 {
     int c;
     int i;
-    //int j;
-    int c25; int coin; int resto; int r;
+    int c25; int coin; int r;
+    printf("Change Calculator\n");
+    printf("I calculate how many coins to give back in change\n");
     do
     {
         c = get_int("Specify the change: ");
@@ -17,11 +18,9 @@ int main(void)
     }
     while (c <= 0);
     i = c % 100;
-    //j = i % 10;
     printf("%i\n", i);
-    //printf("%j\n", j);
-    r = i % 25;   //resto c25
-    coin = i / 25;     //num de moedas
+    r = i % 25;   //remainder c25
+    coin = i / 25;     //number of coins
     printf("Number of 25 cents coins: %d", coin);
     printf ("\n");
     printf("Balance: %d", r);
