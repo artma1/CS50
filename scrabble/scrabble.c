98f6f39c4af5ef8e91483c929b5a86fac82f114c
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-19 19:03:53 +0000

    Tue, Mar 19, 2024, 4:03 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,10 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int N = 26;
+int sum(int N, int alpha[]);
+
 int main(void)
 {
-    stringlengh  a =
     string str1 = get_string("Player 1 insert your word: ");
     string str2 = get_string("Player 2 insert your word: ");
 }
-int aplha( int 26, int )
+
+int sum(int 26, int alpha[])
+{
+    alpha [] = 1, 3, 3, 2, 1, 4;
+    printf(alpha[]);
+}
+
