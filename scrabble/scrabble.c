1c3fd55c294b39c8677a76bb5a6420d123305bd2
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-20 17:56:41 +0000

    Wed, Mar 20, 2024, 2:56 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-//int sum(int N, int alpha[]);
+
 int alpha[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int main(void)
 {
