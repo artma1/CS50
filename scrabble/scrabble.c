c75b472faeb3745de315c919bbe91fbe3001422f
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-20 18:04:27 +0000

    Wed, Mar 20, 2024, 3:04 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -10,12 +10,14 @@ int alpha[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4
 int main(void)
 {
 
-    for (int i =65; i<=91; i++)
-    {
-        printf("%i\n", i);
-    }
+    //for (int i = 65; i <= 90; i++)
+    //{
+     //   printf("%i\n", i);
+    //}
 
     //printf(alpha[2]);
     string str1 = get_string("Player 1 insert your word: ");
     string str2 = get_string("Player 2 insert your word: ");
+    printf("%i", str1[]);
+    printf ("%s", str1);
 }
