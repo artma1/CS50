bfa7c39e614d48459cfd23f7c031b4987396c42f
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-20 17:54:37 +0000

    Wed, Mar 20, 2024, 2:54 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,18 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-const int N = 26;
-
 //int sum(int N, int alpha[]);
-
+int alpha[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int main(void)
 {
-    int alpha[N];
+
     for (int i =65; i<=91; i++)
     {
         printf("%i\n", i);
     }
-    int alpha[N] = 6, 8, 2;
+
     //printf(alpha[2]);
     string str1 = get_string("Player 1 insert your word: ");
     string str2 = get_string("Player 2 insert your word: ");
