cca296d56693331b4acef021d47bb04959ae5d8e
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-20 18:55:43 +0000

    Wed, Mar 20, 2024, 3:55 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,19 +8,34 @@
 // check if uppercase or lowercase
 // detect who is the winner
 // print winner
-int make_score(string word)
+int make_score(string word);
 
 int alpha[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int main(void)
 {
     string str1 = get_string("Player 1 insert your word: ");
     string str2 = get_string("Player 2 insert your word: ");
+    printf("\n");
+    make_score(str1);
+    make_score(str2);
+    if (make_score(str1) > make_score(str2))
+    {
+        printf("Player 1 won!\n");
+    }
+    else if (make_score(str1) < make_score(str2))
+    {
+        printf("Player 2 won!\n");
+    }
+    else
+    printf("Draw!\n");
+    printf("Player 1 score is %i and player 2 score is %i", make_score(str1), make_score(str2));
+}
 
- int make_score(string word)
- {
+int make_score(string word)
+{
     int sum = 0;
     {
-        for (int i = 0; lenght = strlen(word); i < lenght; i++)
+        for (int i = 0; int lenght == strlen(word); i < lenght; i++)
         {
             if (isupper(word[i]))
             {
@@ -32,9 +47,10 @@ int main(void)
             }
             else
             {
-                sum += 0
+                sum += 0;
             }
         }
     }
+    return (sum);
 }
-}
+
