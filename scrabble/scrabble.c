944fcfb80fd20c1b74975995830f902101ee4821
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-20 17:55:51 +0000

    Wed, Mar 20, 2024, 2:55 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -15,10 +15,3 @@ int main(void)
     string str1 = get_string("Player 1 insert your word: ");
     string str2 = get_string("Player 2 insert your word: ");
 }
-
-int sum(int N, int alpha[])
-{
-    alpha [] = {1, 3, 3, 2, 1, 4}
-    printf(alpha[]);
-}
-
