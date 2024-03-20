31aa9ae67744caaeab3651317302dd294d750083
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-20 18:15:25 +0000

    Wed, Mar 20, 2024, 3:15 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,16 +8,11 @@
 // check if uppercase or lowercase
 // detect who is the winner
 // print winner
+ int make_score(string word)
+ 
 int alpha[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int main(void)
 {
-
-    //for (int i = 65; i <= 90; i++)
-    //{
-     //   printf("%i\n", i);
-    //}
-
-    //printf(alpha[2]);
     string str1 = get_string("Player 1 insert your word: ");
     string str2 = get_string("Player 2 insert your word: ");
 
