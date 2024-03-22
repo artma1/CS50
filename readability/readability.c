9e540b7c71b9a8dadcc745a2afc9ff0a0d95b43b
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 12:51:13 +0000

    Fri, Mar 22, 2024, 9:51 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -18,9 +18,9 @@ int test(index);
     int i = 0;
     // L is letters for 100 words
     // S is sentences per 100 words
-   for(i = 0; i = n; i++)
+   for(i = 0; i = \0; i++)
    {
-    
+
    }
    int index = 0.0588 * L - 0.296 * S - 15.8
 }
