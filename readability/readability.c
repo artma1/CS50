2a8771ed5fbdc1350939717f16c87e6f985ac639
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 13:58:12 +0000

    Fri, Mar 22, 2024, 10:58 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -28,7 +28,6 @@ int test(string text)
         if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == ' ')
         {
             nw++;
-            for (nw = 0; nw < 100)
             if( text[i] >= a && <= z || text[i] >= A && <= Z)
             {
                 int L = L++;
