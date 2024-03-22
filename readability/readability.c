d8f12125c26ea22996ccfa391da1a8e62fec4135
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 12:47:31 +0000

    Fri, Mar 22, 2024, 9:47 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,8 +5,19 @@
 // repeat until find character ". ! ? ;" in the end of sentence
 // the array is the size of the entire sentence
 // grade 16 or more, output is "grade 16+", grade 1 or less is "1-"
+int test(index);
+
 int main(void)
 {
-    int text = get_string("Text: ");
-    printf("%s", text);
+    string text = get_string("Text: ");
+    printf("%s\n", text);
+}
+
+int test(index);
+{
+    int i = 0;
+    // L is letters for 100 words
+    // S is sentences per 100 words
+   for(i = 0, )
+   int index = 0.0588 * L - 0.296 * S - 15.8
 }
