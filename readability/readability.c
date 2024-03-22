42ce8d18c525de99716f3e21b378c341e54a86cd
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 12:43:08 +0000

    Fri, Mar 22, 2024, 9:43 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,2 +1,12 @@
 #include <cs50.h>
-#include <>
+#include <stdio.h>
+
+// insert formula
+// repeat until find character ". ! ? ;" in the end of sentence
+// the array is the size of the entire sentence
+// grade 16 or more, output is "grade 16+", grade 1 or less is "1-"
+int main(void)
+{
+    int text = get_string("Text: ");
+    printf("%s", text);
+}
