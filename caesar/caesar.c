56bd83db25058278138fd8b079f0382948fc3399
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 23:30:44 +0000

    Fri, Mar 22, 2024, 8:30 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,16 +4,20 @@
 
 int main(int argc, string argv[])
 {
-    
-    if (isdigit(argv[1]))
+    if(argv[1] == 0)
     {
-        long key = argv[1];
+        printf("Error\n");
+        return (1);
     }
-    else
-    {
-        printf("Usage: ./caesar key\n");
-    }
-    return (1);
+    //if (isdigit(argv[1]))
+    //{
+    //    long key = argv[1];
+    //}
+    //else
+    //{
+    //    printf("Usage: ./caesar key\n");
+    //}
+   // return (1);
     string plain = get_string("plaintext:  ");
     string cypher = get_string("cyphertext: ");
     printf("\n");
