6deddb5bb867ce0f71f72d0e1f878cf4e9978ac8
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 23:17:46 +0000

    Fri, Mar 22, 2024, 8:17 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,13 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
     long key = get_string("Key: "\n);
     if (isdigit(key))
     {
-        
+
     }
+    else
+    return (1);
 
     string plain = get_string("plaintext:  ");
     string cypher = get_string("cyphertext: ");
