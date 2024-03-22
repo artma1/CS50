afb6f8324d67c7b74eee79f4023fbc76b9afea5b
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 23:26:10 +0000

    Fri, Mar 22, 2024, 8:26 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,7 +10,7 @@ int main(int argc, string argv[])
     }
     else
     {
-        printf("Usage: ./caesar key\n")
+        printf("Usage: ./caesar key\n");
     }
     return (1);
     string plain = get_string("plaintext:  ");
