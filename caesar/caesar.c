cd039db2ef6d6dd94e52df7a68d5f678aaf63ccc
artma1 <163445754+artma1@users.noreply.github.com>
2024-03-22 23:11:26 +0000

    Fri, Mar 22, 2024, 8:11 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-    long key = get_string()
+    long key = get_string("Key: "\n);
+    
+
     string plain = get_string("plaintext:  ");
     string cypher = get_string("cyphertext: ");
     printf("\n");
